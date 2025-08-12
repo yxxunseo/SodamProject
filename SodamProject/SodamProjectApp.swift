a06ba7e5f90d261532eadbe0970e8672bf8ee3a7
Yunseo <osope@naver.com>
2025-08-12 14:11:01 +0900

    Initial Commit

---
+++ SodamProject/SodamProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SodamProjectApp.swift
+//  SodamProject
+//
+//  Created by 강윤서 on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SodamProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SodamProjectTests/SodamProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SodamProjectTests.swift
+//  SodamProjectTests
+//
+//  Created by 강윤서 on 8/12/25.
+//
+
+import Testing
+@testable import SodamProject
+
+struct SodamProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

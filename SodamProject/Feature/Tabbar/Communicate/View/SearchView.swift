28e91c77ebddaffd5d7b72df9cf41014eef8c022
Yunseo <osope@naver.com>
2025-09-04 20:39:46 +0900

    Feat:지도 생성 및 뷰 수정

---
+++ SodamProject/Feature/Tabbar/Communicate/View/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  SodamProject
+//
+//  Created by 강윤서 on 9/4/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
---
+++ SodamProject/Feature/Tabbar/Home/View/RoleSelectionModal.swift
@@ -0,0 +1,18 @@
+//
+//  RoleSelectionModal.swift
+//  SodamProject
+//
+//  Created by 강윤서 on 9/3/25.
+//
+
+import SwiftUI
+
+struct RoleSelectionModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoleSelectionModal()
+}
